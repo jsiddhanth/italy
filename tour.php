133080398256739349516b8451f4f972e018cbec
jsiddhanth <55252679+jsiddhanth@users.noreply.github.com>
2020-01-21 08:32:31 +0530

    Add files via upload

---
+++ tour.php
@@ -0,0 +1,376 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Material Design Bootstrap</title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="css/mdb.min.css" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
+  <link href="css/style.min.css" rel="stylesheet">
+  <!-- Stepper Css -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bs-stepper/dist/css/bs-stepper.min.css">
+</head>
+
+<body class="grey lighten-3">
+
+  <!--Main Navigation-->
+  <header>
+
+    <!-- Navbar -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark unique-color scrolling-navbar">
+      <div class="container-fluid">
+
+        <!-- Brand -->
+        <a class="navbar-brand waves-effect" href="#" target="_self">
+          <strong class="white-text">SpeedBird</strong>
+        </a>
+
+        <!-- Collapse -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Links -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+          <!-- Left -->
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link waves-effect" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link waves-effect" href="" target="_blank">visit website</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link waves-effect" href=""target="_blank">currency</a>
+            </li>
+          </ul>
+
+          <!-- Right -->
+          <ul class="navbar-nav nav-flex-icons">
+
+            <li class="nav-item">
+              <a href="" class="nav-link waves-effect" target="_blank">
+                <i class="fab fa-instagram"></i>
+              </a>
+            </li>
+
+          </ul>
+
+        </div>
+
+      </div>
+    </nav>
+    <!-- Navbar -->
+
+    <!-- Sidebar -->
+    <div class="sidebar-fixed position-fixed unique-color">
+
+      <a class="logo-wrapper waves-effect">
+        <img src="img/speed.jpg" class="w-50 z-depth-5 rounded mx-auto d-block">
+      </a>
+
+      <div class="list-group list-group-flush z-depth-5 ">
+        <a href="#" class="list-group-item active waves-effect disabled">
+          <i class="fas fa-compass mr-3"></i>Tours
+        </a>
+        <a href="tour.php" class="list-group-item list-group-item-action waves-effect">
+          <i class="fas fa-user mr-3"></i>create tour</a>
+        <!-- <a href="#" class="list-group-item list-group-item-action waves-effect">
+          <i class="fas fa-map mr-3"></i>display tours</a>
+        <a href="#" class="list-group-item list-group-item-action waves-effect">
+          <i class="fas fa-table mr-3"></i>manage tours</a>
+        <a href="#" class="list-group-item list-group-item-action waves-effect">
+          <i class="fas fa-money-bill-alt mr-3"></i>Bookings</a> -->
+      </div>
+
+    </div>
+    <!-- Sidebar -->
+
+  </header>
+  <!--Main Navigation-->
+
+  <!--Main layout-->
+  <main class="pt-5 mx-lg-5">
+    <div class="container-fluid mt-5">
+
+      <!--Grid row-->
+      <div class="row wow fadeIn justify-content-center">
+
+        <!--Grid column-->
+        <div class="col-md-8 mb-4">
+
+          <!--Card-->
+          <div class="card">
+            
+            <!-- Card header-->
+            <div class="card-header text-center">
+              Create a Tour
+            </div>
+            <!-- /.Card header-->
+            
+            <!--Card content-->
+            <div class="card-body">
+
+            </div>
+
+          </div>
+          <!--/.Card-->
+
+        </div>
+        <!--Grid column-->
+
+      </div>
+      <!--Grid row-->
+
+	  </div>
+    <!--Grid row-->
+      <div class="row wow fadeIn">
+
+        <!--Grid column-->
+        <div class="col-md-6 mb-4">
+
+          <!--Card-->
+          <div class="card">
+
+            <div class="card-header text-center">
+              card one
+            </div>
+            <!--Card content-->
+            <div class="card-body">
+                <form action="#" method="post">
+                
+                  <div class="input-group mb-4">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">title text</span>
+                    </div>
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                  </div>  
+
+                  <div class="input-group mb-4">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">title subtext</span>
+                    </div>
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                  </div> 
+
+                  <div class="input-group mb-4">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">tour full text</span>
+                    </div>
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                  </div> 
+
+                  <div class="input-group mb-4">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">description</span>
+                    </div>
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                  </div> 
+
+                  <div class="input-group mb-4">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">itineray</span>
+                    </div>
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                  </div>
+
+                  <div class="input-group mb-4">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">included</span>
+                    </div>
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                  </div> 
+
+                  <div class="input-group mb-4">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">excluded</span>
+                    </div>
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                  </div> 
+
+                  <div class="input-group mb-4">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">meeting point</span>
+                    </div>
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                  </div> 
+
+                  <div class="input-group mb-4">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">information</span>
+                    </div>
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                  </div> 
+
+                  <div class="input-group mb-4">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">duration</span>
+                    </div>
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                  </div>
+                  
+                </form>
+            </div>
+
+          </div>
+          <!--/.Card-->
+
+        </div>
+        <!--/.Grid column-->
+
+        <!--Grid column-->
+        <div class="col-md-6 mb-4">
+
+          <!--Card-->
+          <div class="card">
+            <div class="card-header text-center">
+              card two
+            </div>
+            <!--Card content-->
+            <div class="card-body">
+              <form action="#" method="post">
+
+              <label for=""> <i class="fa fa-wheelchair" aria-hidden="true"></i>  
+                wheelchair accesable
+              </label>
+                <!-- Group of default radios - option 1 -->
+                <div class="custom-control custom-radio">
+                  <input type="radio" class="custom-control-input" id="defaultGroupExample1" name="groupOfDefaultRadios" checked>
+                  <label class="custom-control-label" for="defaultGroupExample1">no</label>
+                </div>
+
+                <!-- Group of default radios - option 2 -->
+                <div class="custom-control custom-radio">
+                  <input type="radio" class="custom-control-input" id="defaultGroupExample2" name="groupOfDefaultRadios" >
+                  <label class="custom-control-label" for="defaultGroupExample2">yes</label>
+                </div>
+              
+              <hr>
+              <label for=""> <i class="fas fa-hotel"></i>
+                hotel star
+              </label>
+                <!-- Group of default radios - option 1 -->
+                <div class="custom-control custom-radio">
+                  <input type="radio" class="custom-control-input" id="defaultGroupExample1" name="groupOfDefaultRadios" checked>
+                  <label class="custom-control-label" for="defaultGroupExample1"><i class="fas fa-star orange-text"></i></label>
+                </div>
+
+                <!-- Group of default radios - option 2 -->
+                <div class="custom-control custom-radio">
+                  <input type="radio" class="custom-control-input" id="defaultGroupExample2" name="groupOfDefaultRadios" >
+                  <label class="custom-control-label" for="defaultGroupExample2">
+                    <i class="fas fa-star orange-text"></i>
+                    <i class="fas fa-star orange-text"></i>
+                  </label>
+                </div>
+
+                <!-- Group of default radios - option 3 -->
+                <div class="custom-control custom-radio">
+                  <input type="radio" class="custom-control-input" id="defaultGroupExample3" name="groupOfDefaultRadios" >
+                  <label class="custom-control-label" for="defaultGroupExample3">
+                    <i class="fas fa-star orange-text"></i>
+                    <i class="fas fa-star orange-text"></i> 
+                    <i class="fas fa-star orange-text"></i>                   
+                  </label>
+                </div>
+
+                <!-- Group of default radios - option 4 -->
+                <div class="custom-control custom-radio">
+                  <input type="radio" class="custom-control-input" id="defaultGroupExample3" name="groupOfDefaultRadios" >
+                  <label class="custom-control-label" for="defaultGroupExample3">
+                    <i class="fas fa-star orange-text"></i>
+                    <i class="fas fa-star orange-text"></i> 
+                    <i class="fas fa-star orange-text"></i>   
+                    <i class="fas fa-star orange-text"></i>                                       
+                  </label>
+                </div>
+
+                <!-- Group of default radios - option 5 -->
+                <div class="custom-control custom-radio">
+                  <input type="radio" class="custom-control-input" id="defaultGroupExample3" name="groupOfDefaultRadios" >
+                  <label class="custom-control-label" for="defaultGroupExample3">
+                    <i class="fas fa-star orange-text"></i>
+                    <i class="fas fa-star orange-text"></i> 
+                    <i class="fas fa-star orange-text"></i>                   
+                    <i class="fas fa-star orange-text"></i>                   
+                    <i class="fas fa-star orange-text"></i>                   
+                  </label>
+                </div> 
+                <hr>
+                <label for=""> <i class="fas fa-user-edit"></i> group size</label>
+                    <!-- Group of default radios - option 1 -->
+                    <div class="custom-control custom-radio">
+                      <input type="radio" class="custom-control-input" id="defaultGroupExample1" name="groupOfDefaultRadios">
+                      <label class="custom-control-label" for="defaultGroupExample1">
+                        <i class="fas fa-user"></i>
+                        private
+                      </label>
+                    </div>
+
+                    <!-- Group of default radios - option 2 -->
+                    <div class="custom-control custom-radio">
+                      <input type="radio" class="custom-control-input" id="defaultGroupExample2" name="groupOfDefaultRadios">
+                      <label class="custom-control-label" for="defaultGroupExample2">
+                        <i class="fas fa-user-friends"></i>
+                        semi private
+                      </label>
+                    </div>
+
+                    <!-- Group of default radios - option 3 -->
+                    <div class="custom-control custom-radio">
+                      <input type="radio" class="custom-control-input" id="defaultGroupExample3" name="groupOfDefaultRadios" checked>
+                      <label class="custom-control-label" for="defaultGroupExample3">
+                        <i class="fas fa-users"></i>
+                        group
+                      </label>
+                <div class="md-form md-outline">
+                  <input type="text" id="form1" class="form-control">
+                  <label for="form1">Example label</label>
+                </div>
+                    </div>
+              </form>
+            </div>
+
+          </div>
+          <!--/.Card-->
+
+        </div>
+        <!--/.Grid column-->
+
+      </div>
+      <!--/.Grid row-->
+
+  </main>
+  <!--Main layout-->
+
+  <!-- SCRIPTS -->
+  <!-- JQuery -->
+  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bs-stepper/dist/js/bs-stepper.min.js"></script>
+  <!-- Initializations -->
+  <script type="text/javascript">
+
+    // Animations initialization
+    new WOW().init();
+  </script>
+
+</body>
+
+</html>
